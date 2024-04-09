c93ecf0dd25f0cfa1ad1304dfb99cf452ba08f89
ambroisegithub <muhayimana21@gmail.com>
2024-04-09 14:20:55 +0200

    fix

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, laudantium.
+        Cumque, itaque asperiores! Veritatis nobis velit quisquam sunt,
+        perferendis deleniti temporibus cupiditate et quam libero sequi non,
+        architecto, praesentium enim.
+      </p>
+    </div>
+  );
+}
+
+export default App;
