86026a38b8b180c3dc603ac354f53a60fc9f89d9
ambroisegithub <muhayimana21@gmail.com>
2024-04-21 00:13:05 +0200

    improve

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,10 +6,26 @@ import { ThemeContext } from "../../ThemeContext";
 // Adjust the import path as necessary
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+
+import { useState, useEffect } from "react";
 
 function Home() {
   const { theme, toggleTheme } = useContext(ThemeContext);
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/job/jobs/")
+      .then((response) => response.json())
+      .then((data) => {
+        setJobs(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching jobs:", error);
+        setLoading(false);
+      });
+  }, []);
 
   const themeClass =
     theme === "light" ? "bg-[#121721] text-white" : "bg-[#fff] text-white";
@@ -41,6 +57,7 @@ function Home() {
             </div>
           </div>
         </nav>
+        {/* Search bar */}
         <div className="flex w-full justify-center">
           <div className="bg-[#19202D] w-[80%] mt-[-30px] flex flex-row gap-5 justify-around">
             <span className="flex gap-4 h-full border-r-[1px] border-gray-700 w-full">
@@ -72,217 +89,51 @@ function Home() {
         {/* Home */}
         <main className="pr-[30px] pl-[30px] mt-10 w-full p-2 lg:pr-[190px] lg:pl-[190px] md:pr-[70px] md:pl-[70px]">
           <div className="flex flex-wrap pb-0">
-            {/* Job Cards */}
-            <Link to="apply" className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10">
-              <div className="">
-                <div className={`${cardClass} mb-4 h-full rounded-lg`}>
-                  <div className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold">
-                    SCOOT
-                  </div>
-                  <div className="p-6 pl-10">
-                    <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
-                      <span className={textClass}>5h ago</span>
-                      <span>. Full Time</span>
-                    </div>
-                    <div>
-                      <h1
-                        className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Haskell and PureScript Dev
-                      </h1>
-                    </div>
-                    <div>
-                      <p
-                        className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Blogr
-                      </p>
-                    </div>
-                    <div
-                      className={`text-[#5964E0] cursor-pointer ${textClass}`}
-                    >
-                      <p>United State</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-
-            <Link to="apply" className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10">
-              <div className="">
-                <div className={`${cardClass} mb-4 h-full rounded-lg`}>
-                  <div className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold">
-                    SCOOT
-                  </div>
-                  <div className="p-6 pl-10">
-                    <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
-                      <span className={textClass}>5h ago</span>
-                      <span>. Full Time</span>
-                    </div>
-                    <div>
-                      <h1
-                        className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Haskell and PureScript Dev
-                      </h1>
-                    </div>
-                    <div>
-                      <p
-                        className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Blogr
-                      </p>
-                    </div>
-                    <div
-                      className={`text-[#5964E0] cursor-pointer ${textClass}`}
-                    >
-                      <p>United State</p>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              jobs.map((job) => (
+                <div
+                  key={job.id}
+                  className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10"
+                >
+                  <div className="">
+                    <div className={`${cardClass} mb-4 h-full rounded-lg`}>
+                      <img
+                        src={job.logo}
+                        alt={`${job.company} logo`}
+                        className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold"
+                      />
+                      <div className="p-6 pl-10">
+                        <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
+                          <span className={textClass}>{job.postedAt}</span>
+                          <span>. {job.contract}</span>
+                        </div>
+                        <div>
+                          <h1
+                            className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
+                          >
+                            {job.position}
+                          </h1>
+                        </div>
+                        <div>
+                          <p
+                            className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
+                          >
+                            {job.company}
+                          </p>
+                        </div>
+                        <div
+                          className={`text-[#5964E0] cursor-pointer ${textClass}`}
+                        >
+                          <p>{job.location}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-
-            <Link to="apply" className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10">
-              <div className="">
-                <div className={`${cardClass} mb-4 h-full rounded-lg`}>
-                  <div className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold">
-                    SCOOT
-                  </div>
-                  <div className="p-6 pl-10">
-                    <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
-                      <span className={textClass}>5h ago</span>
-                      <span>. Full Time</span>
-                    </div>
-                    <div>
-                      <h1
-                        className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Haskell and PureScript Dev
-                      </h1>
-                    </div>
-                    <div>
-                      <p
-                        className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Blogr
-                      </p>
-                    </div>
-                    <div
-                      className={`text-[#5964E0] cursor-pointer ${textClass}`}
-                    >
-                      <p>United State</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-
-            <Link to="apply" className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10">
-              <div className="">
-                <div className={`${cardClass} mb-4 h-full rounded-lg`}>
-                  <div className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold">
-                    SCOOT
-                  </div>
-                  <div className="p-6 pl-10">
-                    <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
-                      <span className={textClass}>5h ago</span>
-                      <span>. Full Time</span>
-                    </div>
-                    <div>
-                      <h1
-                        className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Haskell and PureScript Dev
-                      </h1>
-                    </div>
-                    <div>
-                      <p
-                        className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Blogr
-                      </p>
-                    </div>
-                    <div
-                      className={`text-[#5964E0] cursor-pointer ${textClass}`}
-                    >
-                      <p>United State</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-
-            <Link to="apply" className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10">
-              <div className="">
-                <div className={`${cardClass} mb-4 h-full rounded-lg`}>
-                  <div className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold">
-                    SCOOT
-                  </div>
-                  <div className="p-6 pl-10">
-                    <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
-                      <span className={textClass}>5h ago</span>
-                      <span>. Full Time</span>
-                    </div>
-                    <div>
-                      <h1
-                        className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Haskell and PureScript Dev
-                      </h1>
-                    </div>
-                    <div>
-                      <p
-                        className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Blogr
-                      </p>
-                    </div>
-                    <div
-                      className={`text-[#5964E0] cursor-pointer ${textClass}`}
-                    >
-                      <p>United State</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-
-            <Link to="apply" className="w-full md:w-1/2 lg:w-1/3 p-4 mb-10">
-              <div className="">
-                <div className={`${cardClass} mb-4 h-full rounded-lg`}>
-                  <div className="flex justify-center text-center items-center bg-yellow-600 w-[70px] h-[70px] mb-2 ml-10 mt-[-30px] rounded-[15px] text-white font-bold">
-                    SCOOT
-                  </div>
-                  <div className="p-6 pl-10">
-                    <div className="flex flex-row gap-3 text-sm text-slate-500 font-bold pb-4">
-                      <span className={textClass}>5h ago</span>
-                      <span>. Full Time</span>
-                    </div>
-                    <div>
-                      <h1
-                        className={`text-white font-bold text-[20px] pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Haskell and PureScript Dev
-                      </h1>
-                    </div>
-                    <div>
-                      <p
-                        className={`text-slate-500 font-bold pb-4 cursor-pointer ${textClass}`}
-                      >
-                        Blogr
-                      </p>
-                    </div>
-                    <div
-                      className={`text-[#5964E0] cursor-pointer ${textClass}`}
-                    >
-                      <p>United State</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            {/* Repeat the job card structure for other jobs */}
+              ))
+            )}
           </div>
           <div className="flex justify-center items-center">
             <button className="bg-[#5964E0] text-white p-3 font-bold rounded-md mb-10 pl-5 pr-5">
